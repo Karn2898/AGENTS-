db079bac06b98dd9ef01035a0ef731358b067b27
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-03-31 15:49:33 +0530

    Add image fetching and conversion functionality
    Fetch images from specified URLs and convert them to RGB format.

--- module II /smolagents/vision agents.py
+++ module II /smolagents/vision agents.py
@@ -1,1 +1,18 @@
+from PIL import Image
+import requests
+from io import BytesI0
 
+image_urls=[
+      "https://upload.wikimedia.org/wikipedia/commons/e/e8/The_Joker_at_Wax_Museum_Plus.jpg", # Joker image
+    "https://upload.wikimedia.org/wikipedia/en/9/98/Joker_%28DC_Comics_character%29.jpg" # Joker image
+]
+
+images=[]
+for url in image_urls:
+  headers={
+    "user-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+  }
+
+response=requests.get(url ,headers=headers)
+image=Image.open(BytesI0(response.content)).convert("RGB")
+images.append(image)
