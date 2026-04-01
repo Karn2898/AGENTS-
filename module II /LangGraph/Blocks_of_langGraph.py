0358f288912473f1c317d283c05b04353784cd29
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-04-02 00:31:36 +0530

    Implement node functions to modify graph state
    Added functions for nodes that update graph state with specific messages.

--- module II /LangGraph/Blocks_of_langGraph.py
+++ module II /LangGraph/Blocks_of_langGraph.py
@@ -3,3 +3,28 @@ from typing_extentions import TypeDict
 
 class State(TypeDict):
   graph_state:str
+#state is user defined
+
+#2. Nodes: takes state as input , performs some operation , returns updates to the state
+
+def node_1(state):
+  return{"graph_state": state['graph_state]+ "I am" }
+
+
+def node_2(state):
+    return{"graph_state": state['grpah_state']+ "happy!"}
+
+def node_3(state):
+   return {"graph_state": state['graph_state]+ "sad !}
+
+"""
+nodes can contain ,
+LLM calls: Generate text or make decisions
+Tool calls: Interact with external systems
+Conditional logic: Determine next steps
+Human intervention: Get input from user"""
+
+
+
+
+    
