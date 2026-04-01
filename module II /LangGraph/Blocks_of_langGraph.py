3164d1fb1d0409c60563216ba776855e692b25cc
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-04-02 00:43:43 +0530

    Correct StateGraph builder and display logic
    Fix typos and improve the StateGraph implementation.

--- module II /LangGraph/Blocks_of_langGraph.py
+++ module II /LangGraph/Blocks_of_langGraph.py
@@ -47,3 +47,27 @@ def decide_mood(state)-> Literal ["node_2","node_3"]
 Direct: Always go from node A to node B
 Conditional: Choose the next node based on the current state"""
 
+
+#4. StateGraph: The container that holds your entire agent workflow
+
+from IPython.display import Image, display
+from langgraph.graph import StateGraph, START, END
+#build graph
+builder =StateGrpah(state)
+builder.add_note("node_1", node_1)
+builder.add_note("node_2", node_2)
+builder.add_note("node_3", node_3)
+
+#logic
+
+builder.add_edge(START ,"node_1")
+builder.add_conditional_edges("node_1",decide_mood)
+builder.add_edge("node_2",END)
+builder.add_edge("node_3",END)
+
+graph=builder.compile()
+
+#view
+display(Imagr(grpah.get_grpah().draw_mermaid_png()))
+graph.invoke({"graph_state" : "Hi, this is Lance."})
+
