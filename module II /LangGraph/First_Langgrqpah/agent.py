d043dbb8e5a89c929b6c749f33f89abf9521cb0f
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-04-03 21:28:18 +0530

    Update agent.py

--- module II /LangGraph/First_Langgrqpah/agent.py
+++ module II /LangGraph/First_Langgrqpah/agent.py
@@ -140,6 +140,45 @@ def notify_mr_hugg(state: EmailState):
     return {}
 
 
+#define routing logic
+def route_email(state: EmailState) -> str:
+    """Determine the next step based on spam classification"""
+    if state["is_spam"]:
+        return "spam"
+    else:
+        return "legitimate"
 
+# Create the graph
+email_graph = StateGraph(EmailState
+
+# Add nodes
+email_graph.add_node("read_email", read_email)
+email_graph.add_node("classify_email", classify_email)
+email_graph.add_node("handle_spam", handle_spam)
+email_graph.add_node("draft_response", draft_response)
+email_graph.add_node("notify_mr_hugg", notify_mr_hugg)
+
+ Start the edges
+email_graph.add_edge(START, "read_email")
+# Add edges - defining the flow
+email_graph.add_edge("read_email", "classify_email")
+
+# Add conditional branching from classify_email
+email_graph.add_conditional_edges(
+    "classify_email",
+    route_email,
+    {
+        "spam": "handle_spam",
+        "legitimate": "draft_response"
+    }
+)
+
+# Add the final edges
+email_graph.add_edge("handle_spam", END)
+email_graph.add_edge("draft_response", "notify_mr_hugg")
+email_graph.add_edge("notify_mr_hugg", END)
+
+# Compile the graph
+compiled_graph = email_graph.compile()
 
 
