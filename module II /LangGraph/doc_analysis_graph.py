3b1c5d24c36f0c9091f303b89e39912d186a4fff
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-04-10 01:03:19 +0530

    Implement assistant function for document analysis
    Added a new assistant function to handle document analysis and computations.

--- module II /LangGraph/doc_analysis_graph.py
+++ module II /LangGraph/doc_analysis_graph.py
@@ -71,3 +71,26 @@ tools=[
 
 llm=ChatOpenAI(model="gpt-4o")
 llm_with_tools = llm.bind_tools(tools, parallel_tool_calls=False)
+
+#nodes
+
+def assistant(state:AgentState):
+  textual_desciption_of_tools="""
+  extract_text(img_path: str) -> str:
+    Extract text from an image file using a multimodal model.
+
+    Args:
+        img_path: A local image file path (strings).
+
+    Returns:
+        A single string containing the concatenated text extracted from each image.
+divide(a: int, b: int) -> float:
+    Divide a and b
+"""
+image=state["input_file"]
+sys_msg=SystemMessage(content=f"you are a helpful butler named Alfred that serves Mr. Wayne and Batman. You can analyse documents and run computations with provided tools:\n{textual_description_of_tool} \n You have access to some optional images. Currently the loaded image is: {image}")
+
+return {
+  "messages": [llm_with_tools.invoke([sys_msg] + state["messages])],
+                                     "input_file":state["input_file"]
+                                     }
