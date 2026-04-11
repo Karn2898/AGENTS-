94de2a3ca8ad01e890e426f21793d8898feb574a
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-04-11 19:17:50 +0530

    Integrate HuggingFace model and tool for guest info

--- Agentic_RAG/RAG_AGENT.py
+++ Agentic_RAG/RAG_AGENT.py
@@ -34,3 +34,48 @@ guest_info_tool=Tool(
   func=extract_text,
   description="Retrieves detailed information about gala guests based on their name or relation."
 )
+#step 3: Integration
+from typing import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+from langchain_core.messages import AnyMessage, HumanMessage, AIMessage
+from langgraph.prebuilt import ToolNode
+from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import tools_condition
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+llm=HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-Coder-32B-Instruct",
+    huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
+)
+
+chat=ChatHuggingFace(llm=llm,verbose=True)
+tools=[guest_info_tool]
+chat_with_tools=chat.bind_tools(tools)
+
+class AgentState(TypeDict):
+  messages:Annonated[list[AnyMessage] , add_messages]
+
+def assistant (state: AgentState):
+  return {
+    "messages":[chat_with_tools.invoke(state["messages"])],
+  }
+#graph
+builder=StateGraph(AgentState)
+
+builder.add_node("assistant",assistant)
+builder.add_node("tools", ToolNode(tools))
+
+# Define edges: these determine how the control flow moves
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    # If the latest message requires a tool, route to tools
+    # Otherwise, provide a direct response
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+alfred = builder.compile()
+
+messages=[HumanMessage(content="Tell me about our guest named 'Lady Ada Lovelace'.")]
+print("Alfred's response :")
+print(response['messages][-1].content)
+
