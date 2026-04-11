bd3f5cd6691afc2f34c306b7f3c67b046c730cb8
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-04-11 19:00:40 +0530

    Implement guest information retrieval tool
    Added a BM25 retriever for querying guest information.

--- Agentic_RAG/RAG_AGENT.py
+++ Agentic_RAG/RAG_AGENT.py
@@ -15,3 +15,22 @@ docs=[
   )
   for guest in guest_dataset
 ]
+
+#create retrieval tool
+from langchain_community.retrievers import BM25Retriever
+from langchain_core.tools import tool
+
+bm25_retriever=BM25Retriever.from_documents(docs)
+
+def extract_text(query:str)->str:
+  results=bm25_retriever.invoke(query)
+ if results:
+   return "\n\n".join([doc.page_content for doc in results[:3]])
+ else:
+   return "no matching"
+
+guest_info_tool=Tool(
+  name="guest_info_retriever",
+  func=extract_text,
+  description="Retrieves detailed information about gala guests based on their name or relation."
+)
