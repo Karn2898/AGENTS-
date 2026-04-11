51d87af6fec96ed47aaed334fb0bd3286af087d8
Tamaghna Sarkar <tamaghna51@gmail.com>
2026-04-11 18:46:41 +0530

    Load dataset and create Document objects
    Load guest dataset and convert to Document objects.

--- Agentic_RAG/RAG_AGENT.py
+++ Agentic_RAG/RAG_AGENT.py
@@ -1,1 +1,17 @@
+import datasets
+from langchain_core.documents import Document
 
+guest_dataset=datasets.load_dataset("agents-course/unit3-invitees", split="train")
+#convert into document objects
+docs=[
+  Document(
+    page_content="\n".join([
+      f"Name: {guest['name']}",
+            f"Relation: {guest['relation']}",
+            f"Description: {guest['description']}",
+            f"Email: {guest['email']}"
+        ]),
+   meadata={"name":guest["name"]} 
+  )
+  for guest in guest_dataset
+]
